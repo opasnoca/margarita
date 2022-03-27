62ec2350fa44ded1b00bf4d27555217b5f5cbf6c
Ivan <opasnoca@gmail.com>
2022-03-27 23:53:13 +0200

    Initial commit  Changes to be committed: 	new file:   .gitattributes 	new file:   .github/workflows/main.yaml 	new file:   .gitignore 	new file:   LICENSE 	new file:   README.md 	new file:   brownie-config.yaml 	new file:   contracts/SafeMath.sol 	new file:   contracts/Token.sol 	new file:   requirements.txt 	new file:   scripts/dispense.py 	new file:   scripts/stake.py 	new file:   scripts/token.py 	new file:   tests/conftest.py 	new file:   tests/test_approve.py 	new file:   tests/test_mint.py 	new file:   tests/test_transfer.py 	new file:   tests/test_transferFrom.py

---
+++ scripts/stake.py
@@ -0,0 +1,18 @@
+from  brownie import Contract, accounts
+from    brownie_tokens import MintableForkToken
+
+def main():
+    dai_addr = "0x6b175474e89094c44da98b954eedeac495271d0f"
+    usdc_addr = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+    registry_addr = "0x0000000022D53366457F9d5E68Ec105046FC4383"
+
+    amount = 100_000 * 10 **18
+    dai = MintableForkToken(dai_addr)
+    dai._mint_for_testing(accounts[0], amount)
+
+    registry = Contract(registry_addr)
+    pool_addr = registry.find_pool_for_coins(dai_addr, usdc_addr)
+    pool = Contract(pool_addr)
+
+    dai.approve(pool_addr, amount, {'from': accounts[0]})
+    pool.add_liquidity([amount, 0, 0], 0, {'from': accounts[0]})
