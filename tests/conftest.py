002a0d171fdf89ac70da058e68f51a006b9d8550
Ivan <opasnoca@gmail.com>
2022-04-09 11:24:44 +0200

    Changes to be committed: 	modified:   tests/conftest.py 	modified:   tests/test_curve.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import pytest
+from brownie import Contract
+from brownie_tokens import MintableForkToken
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -28,3 +30,44 @@ def alice(accounts):
 @pytest.fixture(scope="module")
 def bob(accounts):
     return accounts[1]
+
+
+def load_contract(addr):
+    try:
+        cont = Contract(addr)
+    except ValueError:
+        cont = Contract.from_explorer(addr)
+    return cont
+
+# fixture that calls Curve registry
+@pytest.fixture(scope="module")
+def registry():
+    return  load_contract("0x90E00ACe148ca3b23Ac1bC8C240C2a7Dd9c2d7f5")
+
+# fixture that references other fixture (calls registry and extracts 0 contract which is DAI,USDC,USDT contract(
+@pytest.fixture(scope="module")
+def tripool(registry):
+    return load_contract(registry.pool_list(0))
+
+# fixture that references from regitry fixture and tripool fixture and extracts lp_token contract
+@pytest.fixture(scope="module")
+def tripool_lp_token(registry, tripool):
+    return load_contract(registry.get_lp_token(tripool))
+
+# fixtrue that will mint some DAI as MIntableForkToken give it to alice and fund the tripool of DAI,USDC,USDT)
+@pytest.fixture(scope="module")
+def tripool_funded(registry, alice, tripool):
+    # creates addres for DAI from treepool contract (coin 0 is DAI coin)
+    dai_addr = registry.get_coins(tripool)[0]
+    #create DAI token for exploration
+    dai = MintableForkToken(dai_addr)
+    # create ammount which we wnat ot have
+    amount = 1e21
+    # aprove fuccion for safty as always
+    dai.approve(tripool, amount, {'from': alice})
+    dai._mint_for_testing(alice, amount)
+
+    # add liquditiy comand [DAI, USDC, USDT]
+    amounts = [amount, 0, 0]
+    tripool.add_liquidity(amounts, 0, {'from': alice})
+    return tripool
--- tests/test_curve.py
+++ tests/test_curve.py
@@ -1,2 +1,10 @@
 def test_alice_is_not_bob(alice, bob):
-    assert  alice != bob 
+    assert alice != bob
+
+# test tahat Alice wallet has 0 tokens in tripool cotract
+def test_tripool_initialy_unfunded(tripool_lp_token, alice):
+    assert tripool_lp_token.balanceOf(alice) == 0
+
+# test that tripool contract is funded with DAI coins from alice
+def test_tripool_funded(tripool_lp_token, alice, tripool_funded):
+    assert tripool_lp_token.balanceOf(alice) > 0
